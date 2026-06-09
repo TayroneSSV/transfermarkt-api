0bce448d5d04abb78d2c1310e92eea5682a209c3
TayroneSSV <tayrone.grotz@gmail.com>
2026-06-09 21:40:37 +0200

    Add v2 API router to main API

--- app/api/api.py
+++ app/api/api.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import clubs, competitions, players
+from app.api.v2.api import api_router_v2
 
 api_router = APIRouter()
 api_router.include_router(competitions.router, prefix="/competitions", tags=["competitions"])
 api_router.include_router(clubs.router, prefix="/clubs", tags=["clubs"])
 api_router.include_router(players.router, prefix="/players", tags=["players"])
+
+api_router.include_router(api_router_v2, prefix="/v2")
