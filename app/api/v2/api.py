ae6f03ecca6aaab5b969410ece17e4e3c0fc67e0
TayroneSSV <tayrone.grotz@gmail.com>
2026-06-11 16:59:54 +0200

    Update api.py

--- app/api/v2/api.py
+++ app/api/v2/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.v2.endpoints import players
+from app.api.v2.endpoints import competitions, players
 
 api_router_v2 = APIRouter()
 api_router_v2.include_router(players.router, prefix="/players", tags=["v2 players"])
+api_router_v2.include_router(competitions.router, prefix="/competitions", tags=["v2 competitions"])
